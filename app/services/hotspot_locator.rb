1fa824e07e1c9f90bc693da075c07842496a5553
Bryant Morrill <bryantreadmorrill@gmail.com>
2020-03-29 22:27:04 -0600

    Add evaluate_distance method in HotspotLocator service

--- app/services/hotspot_locator.rb
+++ app/services/hotspot_locator.rb
@@ -1,2 +1,7 @@
 class HotspotLocator
+  def evaluate_distance(point_x, point_y)
+    long_dist = point_x[:long] - point_y[:long]
+    lat_dist = point_x[:lat] - point_y[:lat]
+    Math.sqrt (long_dist * long_dist) + (lat_dist * lat_dist)
+  end
 end
--- test/services/hotspot_locator_test.rb
+++ test/services/hotspot_locator_test.rb
@@ -6,4 +6,14 @@ class HotspotLocatorTest < ActiveSupport::TestCase
     # should not throw an error
     HotspotLocator.new
   end
+
+  def test_hotspot_evaluate_distance
+    point_x = {long: 2, lat: 2}
+    point_y = {long: -2, lat: -2}
+
+    hotspot_loc = HotspotLocator.new
+    result = hotspot_loc.evaluate_distance(point_x, point_y)
+    
+    assert_equal 5.6569, result.round(4)
+  end
 end
