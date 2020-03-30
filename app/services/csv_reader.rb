bb84019608b5f99dba203e92e29ecdbd22dad73f
Bryant Morrill <bryantreadmorrill@gmail.com>
2020-03-29 21:01:39 -0600

    Implement read_from_path method in CsvReader

--- app/services/csv_reader.rb
+++ app/services/csv_reader.rb
@@ -1,2 +1,12 @@
+require 'csv'
+
 class CsvReader
+  def read_from_path(path)
+    @lines = CSV.parse(File.read(path), headers: true)
+  end
+
+  def lines
+    @lines
+  end
+
 end
--- test/services/csv_reader_test.rb
+++ test/services/csv_reader_test.rb
@@ -6,4 +6,15 @@ class CsvReaderTest < ActiveSupport::TestCase
     # should not throw an error
     CsvReader.new
   end
+
+  def test_read_file
+    reader = CsvReader.new
+
+    reader.read_from_path(Rails.root + "test/fixtures/files/test_ufo_sightings.csv")
+
+    assert_equal 6, reader.lines.count
+    assert_equal "This is a test", reader.lines[0]["comments"]
+    assert_equal "san marcos tx (29.8830556:-97.9411111)", reader.lines[0]["Site Location Lat/Lng"]
+  end
+
 end
